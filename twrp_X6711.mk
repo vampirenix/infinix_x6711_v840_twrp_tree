68d6e258d6019d0e4dc645b5b2fab7d52c9fb973
charles_dxd <17maydayparade@gmail.com>
2026-04-19 23:20:55 +0800

    Update twrp_X6711.mk

--- twrp_X6711.mk
+++ twrp_X6711.mk
@@ -13,14 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit userspace reboot properties
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
-# Inherit some common PBRP stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X6711 device
 $(call inherit-product, device/infinix/X6711/device.mk)
 
 PRODUCT_DEVICE := X6711
-PRODUCT_NAME := pb_X6711
+PRODUCT_NAME := twrp_X6711
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6711
 PRODUCT_MANUFACTURER := infinix
